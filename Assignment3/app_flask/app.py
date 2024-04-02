6dfb576aa900b487cd14ae502b5b896597edbadc
Jialin Song <127460254+selenasong@users.noreply.github.com>
2024-04-02 17:25:34 -0400

    Add files via upload

---
+++ Assignment3/app_flask/app.py
@@ -0,0 +1,100 @@
+from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+import ner
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydb.db'
+db = SQLAlchemy(app)
+
+class Token_relation(db.Model):
+   token = db.Column(db.String, primary_key=True)
+   relation = db.Column(db.String, primary_key=True)
+   count = db.Column(db.Integer, primary_key=False, default=1)
+
+   def __init__(self, token, relation):
+      self.token = token
+      self.relation = relation
+
+class Entities(db.Model):
+   entity = db.Column(db.String, primary_key=True)
+   token = db.Column(db.String, primary_key=True)
+   entity_count = db.Column(db.Integer, primary_key=False, default=1)
+
+   def __init__(self, entity, token):
+      self.entity = entity
+      self.token = token
+
+def add_relations_to_db(dependency_parse, entities):
+    with app.app_context():
+        db.create_all()
+        for token, relation, head in dependency_parse:
+            relation_entry = "{} {} {}".format(head, relation, token)
+            existing_relation = Token_relation.query.filter_by(token=token, relation=relation_entry).first()
+            if existing_relation:
+                existing_relation.count += 1
+            else:
+                db.session.add(Token_relation(token=token, relation=relation_entry))
+            db.session.commit()
+
+        for _, _, _, text in entities:
+            existing_entity = Entities.query.filter_by(entity=text).all()
+            if existing_entity:
+                for entity_token in existing_entity:
+                    entity_token.entity_count += 1
+            else:
+                for token in text.split():
+                    db.session.add(Entities(text, token))
+            db.session.commit()
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('form.html', input=open('input.txt').read())
+    else:
+        text = request.form['text']
+        doc = ner.SpacyDocument(text)
+        dependency_parse = doc.get_dependency_parse()
+        entities = doc.get_entities()
+        add_relations_to_db(dependency_parse,entities)
+        markup = doc.get_entities_with_markup()
+        markup_paragraphed = ''
+        for line in markup.split('\n'):
+            if line.strip() == '':
+                markup_paragraphed += '<p/>\n'
+            else:
+                markup_paragraphed += line
+        return render_template('result.html', markup=markup_paragraphed, dependency_parse=dependency_parse, doc=doc)
+
+# alternative where we use two resources
+
+@app.get('/get')
+def index_get():
+    return render_template('form2.html', input=open('input.txt').read())
+
+@app.post('/post')
+def index_post():
+    text = request.form['text']
+    doc = ner.SpacyDocument(text)
+    dependency_parse = doc.get_dependency_parse()
+    entities = doc.get_entities()
+    add_relations_to_db(dependency_parse, entities)
+    markup = doc.get_entities_with_markup()
+    markup_paragraphed = ''
+    for line in markup.split('\n'):
+        if line.strip() == '':
+            markup_paragraphed += '<p/>\n'
+        else:
+            markup_paragraphed += line
+    return render_template('result.html', markup=markup_paragraphed, dependency_parse=dependency_parse, doc=doc)
+
+
+@app.route('/post')
+def show_all():
+    entity_relations = Token_relation.query.join(Entities, Entities.token == Token_relation.token).add_columns(
+         Entities.entity, Entities.entity_count, Token_relation.relation, Token_relation.count).all()
+
+    return render_template('show_all.html', entity_relations=entity_relations)
+
+
+if __name__ == '__main__':
+   app.run(debug=True)
